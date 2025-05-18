b84e524bca87a4a5435041f29128712459343611
Dev Selimcan <selimcangursu@asemsoft.com>
2025-05-19 01:56:51 +0300

    header alanı tasarlandı,responsive hale getirildi.

--- frontend/client/src/components/HeaderBottom.jsx
+++ frontend/client/src/components/HeaderBottom.jsx
@@ -1,13 +1,21 @@
 import React from "react";
 import NavLinks from "./NavLinks";
-import './HeaderBottom.css'
+import "./HeaderBottom.css";
+import { RxHamburgerMenu } from "react-icons/rx";
+import { FaSearch } from "react-icons/fa";
+
 function HeaderBottom() {
   return (
     <div className="header__bottom">
+      <FaSearch id="mobile_search__icon"/>
       <div className="header__bottom__logo">
-        <img src="https://www.bizevdeyokuz.com/wp-content/uploads/bey-logo-v5-retina.png" alt="" />
+        <img
+          src="https://www.bizevdeyokuz.com/wp-content/uploads/bey-logo-v5-retina.png"
+          alt=""
+        />
       </div>
       <NavLinks/>
+      <RxHamburgerMenu id="mobile__sidebar__button" />
     </div>
   );
 }
--- frontend/client/src/components/NavLinks.jsx
+++ frontend/client/src/components/NavLinks.jsx
@@ -1,27 +1,249 @@
 import React from "react";
-import './NavLinks.css'
+import "./NavLinks.css";
+import { FaAngleRight, FaSearch } from "react-icons/fa";
 
 function NavLinks() {
   return (
     <div className="header__bottom__menus">
-      <ul>
+      <ul className="main-menu">
         <li>
-          <a href="">Türkiye</a>
+          <a href="#" className="menu__link">
+            Türkiye
+          </a>
+          <div className="menu__link__main">
+            <ul className="submenu">
+              <li className="submenu__item">
+                <div className="submenu__link__wrapper">
+                  <a href="#" className="submenu__link">
+                    Akdeniz Bölgesi
+                  </a>
+                  <FaAngleRight />
+                </div>
+                <div className="submenu__link__main">
+                  <ul>
+                    <li>
+                      <a href="#">Fransa</a>
+                    </li>
+                    <li>
+                      <a href="#">Almanya</a>
+                    </li>
+                    <li>
+                      <a href="#">İtalya</a>
+                    </li>
+                  </ul>
+                </div>
+              </li>
+              <li className="submenu__item">
+                <div className="submenu__link__wrapper">
+                  <a href="#" className="submenu__link">
+                    Anadolu Bölgesi
+                  </a>
+                  <FaAngleRight />
+                </div>
+                <div className="submenu__link__main">
+                  <ul>
+                    <li>
+                      <a href="#">Fransa</a>
+                    </li>
+                    <li>
+                      <a href="#">Almanya</a>
+                    </li>
+                    <li>
+                      <a href="#">İtalya</a>
+                    </li>
+                  </ul>
+                </div>
+              </li>
+              <li className="submenu__item">
+                <div className="submenu__link__wrapper">
+                  <a href="#" className="submenu__link">
+                    Ege Bölgesi
+                  </a>
+                  <FaAngleRight />
+                </div>
+                <div className="submenu__link__main">
+                  <ul>
+                    <li>
+                      <a href="#">Fransa</a>
+                    </li>
+                    <li>
+                      <a href="#">Almanya</a>
+                    </li>
+                    <li>
+                      <a href="#">İtalya</a>
+                    </li>
+                  </ul>
+                </div>
+              </li>
+              <li className="submenu__item">
+                <div className="submenu__link__wrapper">
+                  <a href="#" className="submenu__link">
+                    Karadeniz Bölgesi
+                  </a>
+                  <FaAngleRight />
+                </div>
+                <div className="submenu__link__main">
+                  <ul>
+                    <li>
+                      <a href="#">Fransa</a>
+                    </li>
+                    <li>
+                      <a href="#">Almanya</a>
+                    </li>
+                    <li>
+                      <a href="#">İtalya</a>
+                    </li>
+                  </ul>
+                </div>
+              </li>
+              <li className="submenu__item">
+                <div className="submenu__link__wrapper">
+                  <a href="#" className="submenu__link">
+                    Marmara Bölgesi
+                  </a>
+                  <FaAngleRight />
+                </div>
+                <div className="submenu__link__main">
+                  <ul>
+                    <li>
+                      <a href="#">Fransa</a>
+                    </li>
+                    <li>
+                      <a href="#">Almanya</a>
+                    </li>
+                    <li>
+                      <a href="#">İtalya</a>
+                    </li>
+                  </ul>
+                </div>
+              </li>
+            </ul>
+          </div>
         </li>
         <li>
-          <a href="">Dünya</a>
+          <a href="#" className="menu__link">
+            Dünya
+          </a>
+          <div className="menu__link__main">
+            <ul className="submenu">
+              <li className="submenu__item">
+                <div className="submenu__link__wrapper">
+                  <a href="#" className="submenu__link">
+                    Avrupa
+                  </a>
+                  <FaAngleRight />
+                </div>
+                <div className="submenu__link__main">
+                  <ul>
+                    <li>
+                      <a href="#">Fransa</a>
+                    </li>
+                    <li>
+                      <a href="#">Almanya</a>
+                    </li>
+                    <li>
+                      <a href="#">İtalya</a>
+                    </li>
+                  </ul>
+                </div>
+              </li>
+              <li className="submenu__item">
+                <div className="submenu__link__wrapper">
+                  <a href="#" className="submenu__link">
+                    Asya
+                  </a>
+                  <FaAngleRight />
+                </div>
+                <div className="submenu__link__main">
+                  <ul>
+                    <li>
+                      <a href="#">Fransa</a>
+                    </li>
+                    <li>
+                      <a href="#">Almanya</a>
+                    </li>
+                    <li>
+                      <a href="#">İtalya</a>
+                    </li>
+                  </ul>
+                </div>
+              </li>
+              <li className="submenu__item">
+                <div className="submenu__link__wrapper">
+                  <a href="#" className="submenu__link">
+                    Amerika
+                  </a>
+                  <FaAngleRight />
+                </div>
+                <div className="submenu__link__main">
+                  <ul>
+                    <li>
+                      <a href="#">Fransa</a>
+                    </li>
+                    <li>
+                      <a href="#">Almanya</a>
+                    </li>
+                    <li>
+                      <a href="#">İtalya</a>
+                    </li>
+                  </ul>
+                </div>
+              </li>
+            </ul>
+          </div>
         </li>
         <li>
-          <a href="">Seyehat Rehberi</a>
+          <a href="#" className="menu__link">
+            Seyahat Rehberi
+          </a>
+          <div className="menu__link__main">
+            <ul>
+              <li>
+                <a href="">Kiralık Evler</a>
+              </li>
+              <li>
+                <a href="">Pasaport</a>
+              </li>
+              <li>
+                <a href="">Vize İşlemleri</a>
+              </li>
+              <li>
+                <a href="">Ulaşım</a>
+              </li>
+            </ul>
+          </div>
+        </li>
+
+        <li>
+          <a href="#" className="menu__link">
+            Aktif Yaşam
+          </a>
+          <div className="menu__link__main">
+            <ul>
+              <li>
+                <a href="">Kiralık Evler</a>
+              </li>
+              <li>
+                <a href="">Pasaport</a>
+              </li>
+              <li>
+                <a href="">Vize İşlemleri</a>
+              </li>
+              <li>
+                <a href="">Ulaşım</a>
+              </li>
+            </ul>
+          </div>
         </li>
         <li>
-          <a href="">LifeStyle</a>
+          <a href="#" className="menu__link">
+            Mağaza
+          </a>
         </li>
         <li>
-          <a href="">Aktif Yaşam</a>
-        </li>
-        <li>
-          <a href="">Mağaza</a>
+          <a href="#" className="menu__link">
+            <FaSearch />
+          </a>
         </li>
       </ul>
     </div>
