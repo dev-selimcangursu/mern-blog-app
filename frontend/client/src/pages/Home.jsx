0db32fd67b19efde4b24738750c069be7e0ec6fc
Dev Selimcan <selimcangursu@asemsoft.com>
2025-05-20 12:17:41 +0300

    home sayfasının css kodları daha düzenli okunabilir hale getirildi.

--- frontend/client/src/pages/Home.jsx
+++ frontend/client/src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./Home.css";
 import Header from "../components/Header";
+import Footer from "../components/Footer";
 import { Splide, SplideSlide } from "@splidejs/react-splide";
 import "@splidejs/react-splide/css";
 import { useEffect } from "react";
@@ -141,6 +142,7 @@ function Home() {
           </div>
         </section>
       </div>
+      <Footer/>
     </>
   );
 }
